18f4384d4ee389b8c30c00b0a8333b3a77886ff5
dvld <nikoroberts620@gmail.com>
2018-10-28 17:50:50 -0500

    working but only first letter shows

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,246 +1,205 @@
-// init hangmanGame object
-var possibleWords = [
+// init object
+let possibleWords = [
+
+  
 
     {
-      img: "not-zelda.jpg",
+      img: 'not-zelda.jpg',
       answer: "Link",
       statement: "His name is Link!",
-      sound:""
+      sound: "assets/sounds/OOT_YoungLink_StrongAttack2.wav"
     },
 
     {
       img: "zelda.png",
       answer: "Zelda",
       statement: "She's Zelda!",
-      sound:""
+      sound: ""
     },
 
-    navi: {
+    {
       img: "navi.jpg",
       answer: "Navi",
       statement: "Hello!?",
-      sound:""
+      sound: ""
     },
 
-    masterSword: {
+    {
       img: "master-sword.png",
       answer: "The Master Sword",
       statement: "The Blade of Evil's Bane",
-      sound:""
+      sound: ""
     },
 
-    rupees: {
+    {
       img: "rupees.jpg",
       answer: "Rupees",
       statement: "Money! Money! Money!",
-      sound:""
+      sound: ""
     },
 
-    epona: {
+    {
       img: "epona.png",
       answer: "Epona",
       statement: "Link's trusty steed",
-      sound:""
+      sound: ""
     },
 
-    goron: {
+    {
       img: "goron.png",
       answer: "Goron",
       statement: "These dude's eat rocks!",
-      sound:""
+      sound: ""
     },
 
-    zora: {
+    {
       img: "zora.png",
       answer: "Zora",
       statement: "Royal fish people",
-      sound:""
-    },
-
-  }
-]
-
-var gameSetup = {
+      sound: ""
+    }
   
+  ];
+
+
+let gameState = {
+  started: false,
+  round: 0,
+  displayString: '',
+  letterGuessed: '',
+  remainingGuesses: 0,
+  wins: 0
+};
+
+let missesAllowed = 9;
+
+// _________________________________________
+
+document.addEventListener('keypress', (event) => {
+  let keyName = event.key.toUpperCase();
+
+  if (gameState.started) {
+    handleGuess(keyName);
+  } else {
+    startGame();
+  }
+});
+
+const handleGuess = (keyName) => {
+  if (
+    gameState.letterGuessed.indexOf(keyName) >= 0
+    ||
+    gameState.displayString.indexOf(keyName) >= 0) {
+    return;
+  }
+
+  let charIndex = possibleWords[gameState.round].answer.indexOf(keyName);
+  if (charIndex >= 0) {
+    correctGuess(keyName);
+  } else {
+    wrongGuess(keyName);
+  }
+
+  updateState();
+
+  gameOver();
+};
+
+const updateState = () => {
+  document.getElementById('word').textContent = addSpaces(gameState.displayString);
+
+  document.getElementById('remainingGuesses').textContent = gameState.remainingGuesses;
+  
+  document.getElementById('lettersGuessed').textContent = addSpaces(gameState.letterGuessed);
+
+  document.getElementById('wins').textcontent = gameState.wins + ' / ' + gameState.round;
+};
+
+const addSpaces = (s) => {
+  let newStr = ''
+  for (i = 0; i < s.length; i++) {
+    newStr += s.charAt(i);
+    newStr += String.fromCharCode(160);
+  }
+
+  return newStr;
 }
 
+const correctGuess = (keyName) => {
+  let newDisplay = '';
+  let word = possibleWords[gameState.round].answer;
 
+  for (i = 0; i < (word.length); i++) {
+    if (word.charAt(i) === keyName) {
+      newDisplay += keyName;
+    } else {
+      newDisplay += gameState.displayString.charAt(i);
+    }
+  }
+  gameState.displayString = newDisplay;
+}
 
+const wrongGuess = (keyName) => {
+  gameState.letterGuessed += keyName;
+  gameState.remainingGuesses--;
+}
 
+const gameOver = () => {
+  // win
+  if (gameState.displayString.indexOf('_') < 0) {
+    console.log('You win!');
 
+    document.getElementById('answer' + gameState.round).innerHTML = possibleWords[gameState.round].answer;
 
+    document.getElementById('img').src = possibleWords[gameState.round].img;
 
+    gameState.round++;
+    gameState.wins++;
+    initializeGame();
 
+    // loss
+  } else if (
+    gameState.letterGuessed.length === missesAllowed
+  ) {
+    alert('Nice Try!, the answer was: ' + possibleWords[gameState.round].answer);
+    gameState.round++;
+    initializeGame();
+  }
+}
 
+const startGame = () => {
+  initializeGame();
+  document.getElementById('pressStart').textContent = '';
+}
 
+const initializeGame = () => {
+  let newWord = possibleWords[gameState.round].answer;
 
+  gameState.started = true;
 
+  gameState.displayString = initDisplay(newWord);
 
+  gameState.remainingGuesses = guessesAllowed(newWord);
 
-//   // init state variables
+  gameState.letterGuessed = '';
 
-//   possiy: null,
-//   lettersOfTheWord: [],
-//   matchedLetters: [],
-//   guessedLetters: [],
-//   guessesLeft: 0,
-//   totalGuesses: 0,
-//   letterGuessed: null,
-//   wins: 0,
+  updateState();
+}
 
-//   // setup game method called on page load
-//   setupGame: function () {
-//     // picks a random word
-//     var objKeys = Object.keys(this.wordsToPick);
-//     this.wordInPlay = objKeys[Math.floor(Math.random() * objKeys.length)];
+const initDisplay = (answer) => {
+  let displayText = '';
 
-//     // splits word into individual letters
-//     this.lettersOfTheWord = this.wordInPlay.split("");
+  for (i = 0; i < answer.length; i++) {
+    if (answer.charAt(i) == ' ') {
+      displayText += ' ';
+    } else {
+      displayText += '_';
+    }
+  }
 
-//     // hidden word representation / letters yet to be guessed appear as underscores ("_ _ _ _")
-//     this.rebuildWordView();
+  return displayText;
+}
 
-//     // sets number of guesses and renders to html
-//     this.processUpdateTotalGuesses();
-//   },
-
-//   // when user guesses a letter
-//   updatePage: function (letter) {
-//     // if no guesses left, restart game
-//     if (this.guessesLeft === 0) {
-//       this.restartGame();
-//     } else {
-//       // check for incorrect guesses
-//       this.updateGuesses(letter);
-
-//       // check for correct guesses
-//       this.updateMatchedLetters(letter);
-
-//       // reveal correct guesses
-//       this.rebuildWordView();
-
-//       // if user wins, restart game
-//       if (this.updateWins() === true) {
-//         this.restartGame();
-//       }
-//     }
-//   },
-
-//   // function for incorrect guesses
-//   updateGuesses: function (letter) {
-//     // if letter is not in guessedLetters or lettersOfTheWord
-//     if ((this.guessedLetters.indexOf(letter) === -1) && (this.lettersOfTheWord.indexOf(letter) === -1)) {
-//       // add to guessedLetters
-//       this.guessedLetters.push(letter);
-
-//       // decrease guesses
-//       this.guessesLeft--;
-
-//       // update guessesLeft and guessedLetters
-//       document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
-//       document.querySelector("#guessed-letters").innerHTML = this.guessedLetters.join(", ");
-//     }
-//   },
-
-//   // sets the init number of guesses
-//   processUpdateTotalGuesses: function () {
-//     // gives the user more guesses the longer the word is
-//     this.totalGuesses = this.lettersOfTheWord.length + 5;
-//     this.guessesLeft = this.totalGuesses;
-
-//     // render guesses left to page
-//     document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
-//   },
-
-//   // function for correct guesses
-//   updateMatchedLetters: function (letter) {
-//     // loop through letters of answer
-//     for (var i = 0; i < this.lettersOfTheWord.length; i++) {
-//       // if letter is in answer and hasn't been guessed
-//       if ((letter === this.lettersOfTheWord[i]) && (this.matchedLetters.indexOf(letter) === -1)) {
-//         // push guessed letter to matchedLetters
-//         this.matchedLetters.push(letter);
-//       }
-//     }
-//   },
-
-//   // function for hidden word display
-//   rebuildWordView: function () {
-//     var wordView = "";
-
-//     for (var i = 0; i < this.lettersOfTheWord.length; i++) {
-//       // if guessed letter is correct, display the letter
-//       if (this.matchedLetters.indexOf(this.lettersOfTheWord[i]) !== -1) {
-//         wordView += this.lettersOfTheWord[i];
-
-//         // if letter hasn't been guessed, display "_"
-//       } else {
-//         wordView += "&nbsp;_&nbsp;";
-//       }
-//     }
-
-//     // update the page with new string
-//     document.querySelector("#current-word").innerHTML = wordView;
-//   },
-
-//   // function to restart game
-//   restartGame: function() {
-//     document.querySelector("#guessed-letters").innerHTML = "";
-//     this.wordInPlay = null;
-//     this.lettersOfTheWord = [];
-//     this.matchedLetters = [];
-//     this.guessedLetters = [];
-//     this.guessesLeft = 0;
-//     this.totalGuesses = 0;
-//     this.letterGuessed = null;
-//     this.setupGame();
-//     this.rebuildWordView();
-//   },
-
-//   // function to see if user has won
-//   updateWins: function() {
-//     var win;
-
-//     // if user hasn't guessed word, set win to false
-//     if (this.matchedLetters.length === 0) {
-//       win = false;
-//     } else {
-//       win = true;
-//     }
-
-//     for (var i = 0; i < this.lettersOfTheWord.length; i++) {
-//       if (this.matchedLetters.indexOf(this.lettersOfTheWord[i]) === -1) {
-//         win = false;
-//       }
-//     }
-
-//     // if win is true
-//     if (win) {
-//       // increment wins
-//       this.wins = this.wins + 1;
-
-//       // update wins on the page
-//       document.querySelector("#wins").innerHTML = this.wins;
-
-//       // update answer text and image
-//       document.querySelector("#game-image").innerHTML = this.wordsToPick[this.wordInPlay].answer + ": " + this.wordInPlay;
-//       document.querySelector("#game-div").innerHTML = "<img class='game-image' src='assets/images/" + this.wordsToPick[this.wordInPlay].picture + "' alt='" + this.wordsToPick[this.wordInPlay].answer + "'>";
-
-//       // return true to start game
-//       return true;
-//     }
-//     // if win is false
-//     return false;
-//   }
-// };
-
-// // init game when page loads
-// hangmanGame.setupGame();
-
-// // when a key is pressed
-// document.onkeyup = function(event) {
-//   // capture key and make it lowercase
-//   hangmanGame.letterGuessed = String.fromCharCode(event.which).toLowerCase();
-//   // pass guessed letter into updatePage function to run logic
-//   hangmanGame.updatePage(hangmanGame.letterGuessed);
-// };
+const guessesAllowed = () => {
+  return missesAllowed;
+};
